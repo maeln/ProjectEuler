25bf3e029a8af4f040a32e65cb76d15a48f41534
Maël NACCACHE <contact@maeln.com>
2017-04-20 14:42:46 +0200

    Simplifed solution for exo 12.

--- exo12.rkt
+++ exo12.rkt
@@ -1,20 +1,12 @@
 #lang racket
 
-(define (divisors n)
-  (filter (lambda (x) (= 0 (modulo n x))) (range 1 (+ n 1))))
 
-;(define (tri-nums n)
-;  (for/last ([i (range 1 (+ n 1))])
-;    (apply + (range 1 (+ i 1)))))
+(require math)
 
 (define cnt 0)
 (define (tnums! n)
   (set! cnt (+ cnt n))
   cnt)
-
-;(time (for/last ([i (in-naturals 1)])
-;  #:final (> (length (divisors (tri-nums i))) 100)
-;  (tri-nums i)))
 
 (time (for/last ([i (in-naturals 1)])
   #:final (> (length (divisors (tnums! i))) 500)
